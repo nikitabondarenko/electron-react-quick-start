13dfc545b749c62761f0cd94e531ff681b24b455
nikitabondarenko <nob2107@columbia.edu>
2017-10-30 15:41:11 -0700

    added text box and save button

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+//import App from './components/App.js';
+import Editor from './components/Editor.js';
+
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
 
-ReactDOM.render(<p>React lives!</p>,
+ReactDOM.render(<Editor/>,
    document.getElementById('root'));
--- reactApp/components/App.js
+++ reactApp/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Editor, EditorState} from 'draft-js';
-// import AppStyle from '../styles/application';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+import AppStyle from '../styles/application';
 
 export default class App extends React.Component {
   constructor(props){
@@ -8,13 +8,19 @@ export default class App extends React.Component {
     this.state = {editorState: EditorState.createEmpty()};
     this.onChange = (editorState) => this.setState({editorState});
   }
-
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'BOLD'
+    ));
+  }
 
   render() {
     return (
       <div id='content'>
         <h1>Draft.js Editor</h1>
         {/* <AppStyle /> */}
+        <button onClick={() => this._onBoldClick(this)}>Bold</button>
         <div className='editor'>
           <Editor
             editorState={this.state.editorState}
