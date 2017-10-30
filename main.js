0e6acc5265604370e00651e187a0e4b028633d59
bvi1994 <bvi1@ucsc.edu>
2017-10-30 14:24:19 -0700

    FOr Nick

--- main.js
+++ main.js
@@ -34,8 +34,8 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
